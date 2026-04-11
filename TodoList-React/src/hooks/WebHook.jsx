3ef138c2600e6947658b21d628809db98a8f7d0a
Igor Torres <igortorres234@gmail.com>
2026-04-11 14:38:51 -0300

    WebHook ajuste

--- TodoList-React/src/hooks/WebHook.jsx
+++ TodoList-React/src/hooks/WebHook.jsx
@@ -8,11 +8,11 @@ const WebHook = ({ title, description, color, properties }) => {
             const webhookUrl = 'https://discord.com/api/webhooks/1441113413878026301/dpuZeDbFuPXZeF5R2sjhB9sL-hsKSvwr6Dm5LdS1ZUauRhVRFE2l5v7aVbTrBFIeiHmY';
 
             const payload = {
-                username: 'Dev [Squirrel-ADM]',
-            avatar_url: 'https://cdn.discordapp.com/attachments/1441061340197158944/1441061400905781459/15036608_191927481265760_7120102291578567324_n.jpg?ex=69206c83&is=691f1b03&hm=df1da1877c787751163a74a21f3faac6a2fe11cbc088e31b69c365bc6ee8a00b&', // Optional: Custom avatar for the bot
+                username: 'Page Quiz Bot TodoList', // Optional: Custom username for the bot
+                avatar_url: 'https://cdn.discordapp.com/attachments/1441061340197158944/1492579365970514116/TodoList-0.png?ex=69dbd85b&is=69da86db&hm=cdc4cdf5674c2f63138e14a418680b3b4535cd85a4a7955d7286f3566bb0957a&', // Optional: Custom avatar for the bot
                 content: 'Callback from Page Quiz Privacy Policy',
                 embeds: [{
-                    title: title,
+                    title: title,       
                     description: description,
                     color: parseInt(color) || 0,
                     fields: [
