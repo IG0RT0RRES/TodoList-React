2c39bbcef944e711e77da01371ab809c8cc9cd50
Igor Torres <45331732+IG0RT0RRES@users.noreply.github.com>
2026-08-04 10:36:49 -0300

    Add Payments component route to App

--- TodoList-React/src/App.jsx
+++ TodoList-React/src/App.jsx
@@ -4,6 +4,7 @@ import PrivacyPolicy from './components/PrivacyPolicy'
 import DeveloperProfile from './components/DeveloperProfile'
 import AuthForm from './components/AuthForm'
 import LeaderBoard from './components/LeaderBoard'
+import Payments from './components/Payments'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
               <Route path="*" element={<h1>404 - Página Não Encontrada</h1>} />
               <Route path="/Cadastro" element={<AuthForm/>}/>
               <Route path="/LeaderBoard" element={<LeaderBoard/>}/>
+              <Route path="/Pagamentos" element={<Payments/>}/>
             </Routes>
           </BrowserRouter>
         </div>)
