ff2a7ea9cb3799d9d2e0b2d17ec812a12bac6794
Igor Torres <igortorres234@gmail.com>
2026-08-09 22:05:39 -0300

    Adicionado o tailwindcss no projeto para a page exclusão de dados

--- TodoList-React/src/main.jsx
+++ TodoList-React/src/main.jsx
@@ -6,6 +6,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+// 👇 Adicione esta linha importando o CSS do Tailwind
+import './css/index.css' // (ou './css/index.css', dependendo de onde criou o arquivo)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
--- TodoList-React/vite.config.js
+++ TodoList-React/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   define: {
     global: 'window',
     'process.env': {},
-    Buffer: ['buffer', 'Buffer'], 
+    Buffer: ['buffer', 'Buffer'],
   },
 })
